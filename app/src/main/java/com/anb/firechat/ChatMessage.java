9cca6157024ad54da66afe5f68a9f53b3f88ff2d
Agung Nursatria <agungnursatria@gmail.com>
2018-03-11 15:36:56 +0700

    Initial commit

---
+++ app/src/main/java/com/anb/firechat/ChatMessage.java
@@ -0,0 +1,47 @@
+package com.anb.firechat;
+
+import java.util.Date;
+
+/**
+ * Created by Agung Nursatria on 3/11/2018.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
